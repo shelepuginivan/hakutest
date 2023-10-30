6f3b550cbbef86c9c483d5e1ed779945dee6d670
shelepuginivan <shelepuginivanm@gmail.com>
2023-10-30 19:19:30 +0300

    feat(server): read port from config

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shelepuginivan/hakutest/internal/config"
 	"github.com/spf13/cobra"
 )
 
 func Init(cmd *cobra.Command, args []string) error {
 	r := gin.Default()
+	config := config.Init()
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -16,5 +18,5 @@ func Init(cmd *cobra.Command, args []string) error {
 		})
 	})
 
-	return r.Run()
+	return r.Run(":" + config.Port)
 }
