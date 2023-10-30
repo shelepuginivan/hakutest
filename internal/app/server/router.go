9dc1d7cbde3247741bc458f87c99d38e721b9568
shelepuginivan <shelepuginivanm@gmail.com>
2023-10-30 21:33:18 +0300

    feat(server): register test controller

---
+++ internal/app/server/router.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shelepuginivan/hakutest/internal/app/server/controllers"
+)
+
+func NewRouter() *gin.Engine {
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+	test := new(controllers.TestController)
+	router.GET("/:test", test.GetTest)
+
+	return router
+}
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -1,22 +1,12 @@
 package server
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 	"github.com/shelepuginivan/hakutest/internal/config"
 	"github.com/spf13/cobra"
 )
 
 func Init(cmd *cobra.Command, args []string) error {
-	r := gin.Default()
+	r := NewRouter()
 	config := config.Init()
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-
 	return r.Run(":" + config.Port)
 }
