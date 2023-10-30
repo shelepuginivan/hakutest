1afbdbb7b292dc35f4abe898038645a653a2a172
shelepuginivan <shelepuginivanm@gmail.com>
2023-10-30 21:32:53 +0300

    feat(app, controllers): add test controller

---
+++ internal/app/server/controllers/test.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	parser "github.com/shelepuginivan/hakutest/internal/pkg/test_parser"
+)
+
+type TestController struct{}
+
+func (t TestController) GetTest(c *gin.Context) {
+	testName := c.Param("test")
+	test, err := parser.ParseTest(testName)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			c.JSON(http.StatusNotFound, gin.H{"detail": "test file does not exist"})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"detail": "failed to parse test file"})
+		}
+
+		return
+	}
+
+	c.JSON(http.StatusOK, test)
+}
