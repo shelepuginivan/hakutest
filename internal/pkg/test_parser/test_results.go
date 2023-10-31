2b05f2b394b3783a2bea5cdea47b322802350e57
shelepuginivan <shelepuginivanm@gmail.com>
2023-10-31 17:54:54 +0300

    feat(test_parser): add more data in test results

--- internal/pkg/test_parser/test_results.go
+++ internal/pkg/test_parser/test_results.go
@@ -1,17 +1,34 @@
 package parser
 
 import (
-	"fmt"
+	"os"
+	"path"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/shelepuginivan/hakutest/internal/config"
+	"gopkg.in/yaml.v3"
 )
 
 type TestResults struct {
-	Student     string
-	SubmittedAt time.Time
-	Points      int
-	Percentage  int
+	Student     string    `yaml:"student"`
+	SubmittedAt time.Time `yaml:"submittedAt"`
+	Results     Results   `yaml:"results"`
+	Test        TestInfo  `yaml:"test"`
+}
+
+type Results struct {
+	Points     int             `yaml:"points"`
+	Total      int             `yaml:"total"`
+	Percentage int             `yaml:"percentage"`
+	Tasks      map[string]bool `yaml:"tasks"`
+}
+
+type TestInfo struct {
+	Title  string `yaml:"title"`
+	Author string `yaml:"author"`
+	Sha256 string `yaml:"sha256"`
 }
 
 func CompareAnswers(received, expected string) bool {
@@ -19,16 +36,35 @@ func CompareAnswers(received, expected string) bool {
 }
 
 func GetTestResults(name string, answers map[string][]string) (TestResults, error) {
-	results := TestResults{}
-	test, err := ParseTest(name)
 	submittedAt := time.Now()
-	points := 0
+	student := strings.Join(answers["student"], "")
+	test, err := ParseTest(name)
 
 	if err != nil {
-		return results, err
+		return TestResults{}, err
 	}
 
-	student := strings.Join(answers["student"], "")
+	checksum, err := GetTestCheckSum(name)
+
+	if err != nil {
+		return TestResults{}, err
+	}
+
+	results := TestResults{
+		Student:     student,
+		SubmittedAt: submittedAt,
+		Test: TestInfo{
+			Title:  test.Title,
+			Author: test.Author,
+			Sha256: checksum,
+		},
+		Results: Results{
+			Points:     0,
+			Total:      len(test.Tasks),
+			Percentage: 0,
+			Tasks:      make(map[string]bool),
+		},
+	}
 
 	for i, answer := range answers {
 		index, err := strconv.Atoi(i)
