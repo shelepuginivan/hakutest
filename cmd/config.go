5333b0532a51940834f00dfa9afb55884411eac7
shelepuginivan <shelepuginivanm@gmail.com>
2023-10-31 20:16:50 +0300

    feat(cmd): add command `config`

---
+++ cmd/config.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/shelepuginivan/hakutest/internal/config"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(configCmd)
+}
+
+var configCmd = &cobra.Command{
+	Use:   "config",
+	Short: "Manage configuration",
+	Long:  "Manage configuration for hakutest",
+	Args:  cobra.RangeArgs(0, 2),
+	Run:   config.ConfigCmd,
+}
