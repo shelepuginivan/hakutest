390140fa4d05f59bc38a78d223150c5f2ada98e8
shelepuginivan <shelepuginivanm@gmail.com>
2023-11-03 19:57:06 +0300

    feat(web): add editor page

---
+++ web/static/js/editor.js
@@ -0,0 +1,108 @@
+const tasksSection = document.getElementById("tasks")
+const buttonAddTask = document.getElementById("button-add-task")
+const numberOfTasksInput = document.getElementById("number-of-tasks-input")
+
+let taskIndex = 0
+
+const addTask = (taskIndex) => {
+    const newTask = `
+        <fieldset class="test-task task-${taskIndex}">
+            <legend>Task ${taskIndex + 1}</legend>
+            <select class="input-select" name="${taskIndex}-type">
+                <option value="single">Single answer</option>
+                <option value="multiple">Multiple answers</option>
+                <option value="open">Open question</option>
+            </select>
+            <input
+                class="input-text"
+                type="text"
+                name="${taskIndex}-text"
+            >
+            <input
+                class="input-text"
+                type="text"
+                name="${taskIndex}-answer"
+            >
+            <div>
+                <input
+                    class="input-checkbox"
+                    type="checkbox"
+                    name="${taskIndex}-has-attachment"
+                    onchange="toggleAttachment(${taskIndex}, this)"
+                >
+                <div
+                    id="${taskIndex}-attachment"
+                    class="attachment"
+                    data-enabled="false"
+                ></div>
+                <div>
+                    <div id="${taskIndex}-options"></div>
+                    <button
+                        class="button-add-option"
+                        type="button"
+                        onclick="addOption(${taskIndex})"
+                    >
+                        + Add option
+                    </button>
+                </div>
+            </div>
+        </fieldset>`
+
+    tasksSection.innerHTML += newTask
+}
+
+const addAttachment = (taskIndex) => {
+    const attachment = document.getElementById(`${taskIndex}-attachment`)
+    const attachmentFields = `
+        <input
+            class="input-text"
+            type="text"
+            name="${taskIndex}-attachment-name"
+        >
+        <select class="input-select" name="${taskIndex}-attachment-type">
+            <option value="file">File</option>
+            <option value="image">Image</option>
+            <option value="video">Video</option>
+            <option value="audio">Audio</option>
+        </select>
+        <input
+            class="input-text"
+            type="text"
+            name="${taskIndex}-attachment-src"
+        >`
+
+    attachment.innerHTML = attachmentFields
+    attachment.dataset.enabled = true
+}
+
+const removeAttachment = (taskIndex) => {
+    const attachment = document.getElementById(`${taskIndex}-attachment`)
+    attachment.innerHTML = ""
+    attachment.dataset.enabled = false
+}
+
+const toggleAttachment = (taskIndex, checkbox) => {
+    if (!checkbox.checked) {
+        return removeAttachment(taskIndex)
+    }
+
+    addAttachment(taskIndex)
+}
+
+const addOption = (taskIndex) => {
+    const taskOptions = document.getElementById(`${taskIndex}-options`)
+    const newOption = `
+        <input
+            class="input-text"
+            type="text"
+            name="${taskIndex}-options"
+        >
+    `
+
+    taskOptions.innerHTML += newOption
+}
+
+buttonAddTask.onclick = () => {
+    addTask(taskIndex)
+    numberOfTasksInput.value = ++taskIndex
+}
