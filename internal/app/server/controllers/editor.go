29a8ee604480e65b4f29c5a3dda95fae9ba646d1
shelepuginivan <shelepuginivanm@gmail.com>
2023-11-03 19:59:28 +0300

    feat(editor): handle test creation submit

--- internal/app/server/controllers/editor.go
+++ internal/app/server/controllers/editor.go
@@ -2,8 +2,11 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/shelepuginivan/hakutest/internal/config"
@@ -91,4 +94,79 @@ func (e EditorController) UploadTest(c *gin.Context) {
 		"Config": config.Init(),
 		"Test":   test,
 	})
+}
+
+func (e EditorController) CreateTest(c *gin.Context) {
+	test := parser.Test{}
+	err := c.Request.ParseMultipartForm(1000)
+
+	if err != nil {
+		c.HTML(http.StatusUnprocessableEntity, "error.tmpl", gin.H{
+			"Config": config.Init(),
+			"Code":   http.StatusUnprocessableEntity,
+			"Detail": "failed to parse form",
+			"Error":  err.Error(),
+		})
+
+		return
+	}
+
+	expiresIn, err := time.Parse("2006-01-02T15:04:05", c.Request.Form.Get("expiresIn"))
+
+	if err == nil {
+		test.ExpiresIn = expiresIn
+	}
+
+	test.Title = c.Request.Form.Get("title")
+	test.Description = c.Request.Form.Get("description")
+	test.Subject = c.Request.Form.Get("subject")
+	test.Target = c.Request.Form.Get("target")
+	test.Title = c.Request.Form.Get("title")
+	test.Author = c.Request.Form.Get("author")
+	test.Institution = c.Request.Form.Get("institution")
+
+	numberOfTasks, err := strconv.Atoi(c.Request.Form.Get("number-of-tasks"))
+
+	if err != nil {
+		numberOfTasks = 0
+	}
+
+	for i := 0; i < numberOfTasks; i++ {
+		task := parser.Task{}
+
+		task.Type = c.Request.Form.Get(fmt.Sprintf("%d-type", i))
+		task.Text = c.Request.Form.Get(fmt.Sprintf("%d-text", i))
+		task.Answer = c.Request.Form.Get(fmt.Sprintf("%d-answer", i))
+		task.Options = c.PostFormArray(fmt.Sprintf("%d-options", i))
+
+		if c.Request.Form.Get(fmt.Sprintf("%d-has-attachment", i)) == "on" {
+			attachment := parser.Attachment{}
+
+			attachment.Type = c.Request.Form.Get(fmt.Sprintf("%d-attachment-type", i))
+			attachment.Name = c.Request.Form.Get(fmt.Sprintf("%d-attachment-name", i))
+			attachment.Src = c.Request.Form.Get(fmt.Sprintf("%d-attachment-src", i))
+
+			task.Attachment = attachment
+		}
+
+		test.Tasks = append(test.Tasks, task)
+	}
+
+	data, err := json.Marshal(test)
+
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "error.tmpl", gin.H{
+			"Config": config.Init(),
+			"Code":   http.StatusBadRequest,
+			"Detail": "failed to create a test file",
+			"Error":  err.Error(),
+		})
+
+		return
+	}
+
+	c.Header("Content-Disposition", fmt.Sprintf("attachment; filename=%s.json", test.Title))
+	c.Header("Content-Type", "application/json")
+	c.Status(http.StatusCreated)
+	c.Writer.Write(data)
 }
--- internal/app/server/router.go
+++ internal/app/server/router.go
@@ -17,6 +17,7 @@ func NewRouter() *gin.Engine {
 
 	router.GET("/editor/upload", editor.ChooseTest)
 	router.POST("/editor/edit", editor.UploadTest)
+	router.POST("/editor/create", editor.CreateTest)
 	router.GET("/:test", test.GetTest)
 	router.POST("/:test", test.SubmitTest)
 
