0bea29225e7fdd22af0314757c84f98790da8ea2
shelepuginivan <shelepuginivanm@gmail.com>
2023-11-04 13:39:22 +0300

    feat(config): add config helper methods
    Add config methods:
    
    - `Print` - print configuration
    - `PrintField` - print the specified configuration field
    - `SetField` - set the specified configuration field

--- internal/config/config.go
+++ internal/config/config.go
@@ -140,3 +140,92 @@ func Init() Config {
 
 	return config
 }
+
+func (c Config) Print() {
+	fmt.Println("genaral")
+	c.PrintField("general")
+	fmt.Println()
+	fmt.Println("server")
+	c.PrintField("server")
+	fmt.Println()
+	fmt.Println("ui")
+	c.PrintField("ui")
+}
+
+func (c Config) PrintField(field string) {
+	switch field {
+	case "general":
+		tbl := table.New("Key", "Value")
+		tbl.AddRow("tests_directory", c.General.TestsDirectory)
+		tbl.AddRow("results_directory", c.General.ResultsDirectory)
+		tbl.Print()
+	case "general.tests_directory":
+		fmt.Println(c.General.TestsDirectory)
+	case "general.results_directory":
+		fmt.Println(c.General.ResultsDirectory)
+	case "server":
+		tbl := table.New("Key", "Value")
+		tbl.AddRow("port", c.Server.Port)
+		tbl.Print()
+	case "server.port":
+		fmt.Println(c.Server.Port)
+	case "ui":
+		tbl := table.New("Key", "Value")
+		tbl.AddRow("error.error_header_label", c.Ui.Error.ErrorHeaderLabel)
+		tbl.AddRow("error.error_details_label", c.Ui.Error.ErrorDetailsLabel)
+		tbl.AddRow("test.open_answer_label", c.Ui.Test.OpenAnswerLabel)
+		tbl.AddRow("test.student_name_label", c.Ui.Test.StudentNameLabel)
+		tbl.AddRow("test.submit_button_label", c.Ui.Test.SubmitButtonLabel)
+		tbl.Print()
+	case "ui.error":
+		tbl := table.New("Key", "Value")
+		tbl.AddRow("error_header_label", c.Ui.Error.ErrorHeaderLabel)
+		tbl.AddRow("error_details_label", c.Ui.Error.ErrorDetailsLabel)
+		tbl.Print()
+	case "ui.error.error_header_label":
+		fmt.Println(c.Ui.Error.ErrorHeaderLabel)
+	case "ui.error.error_details_label":
+		fmt.Println(c.Ui.Error.ErrorDetailsLabel)
+	case "ui.test":
+		tbl := table.New("Key", "Value")
+		tbl.AddRow("open_answer_label", c.Ui.Test.OpenAnswerLabel)
+		tbl.AddRow("student_name_label", c.Ui.Test.StudentNameLabel)
+		tbl.AddRow("submit_button_label", c.Ui.Test.SubmitButtonLabel)
+		tbl.Print()
+	case "ui.test.open_answer_label":
+		fmt.Println(c.Ui.Test.OpenAnswerLabel)
+	case "ui.test.student_name_label":
+		fmt.Println(c.Ui.Test.StudentNameLabel)
+	case "ui.test.submit_button_label":
+		fmt.Println(c.Ui.Test.SubmitButtonLabel)
+	default:
+		log.Fatal("unknown field:", field)
+	}
+}
+
+func (c *Config) SetField(field, value string) error {
+	switch field {
+	case "general.tests_directory":
+		c.General.TestsDirectory = value
+	case "general.results_directory":
+		c.General.ResultsDirectory = value
+	case "server.port":
+		c.Server.Port = value
+	case "ui.error.error_header_label":
+		c.Ui.Error.ErrorHeaderLabel = value
+	case "ui.error.error_details_label":
+		c.Ui.Error.ErrorDetailsLabel = value
+	case "ui.test.open_answer_label":
+		c.Ui.Test.OpenAnswerLabel = value
+	case "ui.test.student_name_label":
+		c.Ui.Test.StudentNameLabel = value
+	case "ui.test.submit_button_label":
+		c.Ui.Test.SubmitButtonLabel = value
+	case "general", "server", "ui", "ui.error", "ui.test":
+		return errors.New("can only set primitive values")
+	default:
+		return errors.New("unknown field: " + field)
+	}
+
+	return nil
+}
