ada43d939c024dd45ed9d3a861e7ecb32ed850ad
shelepuginivan <shelepuginivanm@gmail.com>
2023-11-17 22:52:35 +0300

    feat(website, docusaurus): add sitemap plugin

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -44,6 +44,12 @@ const config: Config = {
                 theme: {
                     customCss: './src/css/custom.css',
                 },
+                sitemap: {
+                    changefreq: 'weekly',
+                    priority: 0.5,
+                    ignorePatterns: ['/tags/**'],
+                    filename: 'sitemap.xml',
+                },
             } satisfies Options,
         ],
     ],
