505133b6033989d2ef6dc1ea026e838490cf5d7d
shelepuginivan <shelepuginivanm@gmail.com>
2023-11-17 23:47:46 +0300

    feat(website, pages): change homepage title and description

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,10 +5,11 @@ import HomepageHeader from '@site/src/components/HomepageHeader'
 
 const Home = () => {
     const { siteConfig } = useDocusaurusContext()
+
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />"
+            title={siteConfig.tagline}
+            description={`${siteConfig.title} - ${siteConfig.tagline}`}
         >
             <HomepageHeader />
         </Layout>
