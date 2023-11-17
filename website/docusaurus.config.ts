ef08d2465cccdca0b920c7ffcf5ee576aa1126c4
shelepuginivan <shelepuginivanm@gmail.com>
2023-11-17 22:41:11 +0300

    feat(website, config): change edit urls

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -33,17 +33,13 @@ const config: Config = {
             {
                 docs: {
                     sidebarPath: './sidebars.js',
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/shelepuginivan/hakutest/tree/main/website',
                 },
                 blog: {
                     showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/shelepuginivan/hakutest/tree/main/website',
                 },
                 theme: {
                     customCss: './src/css/custom.css',
