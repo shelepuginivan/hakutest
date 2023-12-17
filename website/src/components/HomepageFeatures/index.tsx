6a6b29eb615daf11c111c3510fed882c2fd43923
shelepuginivan <shelepuginivanm@gmail.com>
2023-12-17 10:01:47 +0300

    feat(i18n, ru): add HomepageFeatures component translations

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -1,38 +1,49 @@
 import Feature, { FeatureProps } from '@site/src/components/Feature'
 
 import styles from './styles.module.css'
+import Translate, { translate } from '@docusaurus/Translate'
 
 const HomepageFeatures = () => {
     const features: FeatureProps[] = [
         {
-            title: 'Efficient',
-            description:
-                'Hakutest is written in Go, making it a high-performance system.',
+            title: translate({ message: 'Efficient' }),
+            description: translate({
+                message:
+                    'Hakutest is written in Go, making it a high-performance system.',
+            }),
             Svg: require('@site/static/img/icon-speed.svg').default,
         },
         {
-            title: 'Secure',
-            description:
-                'Your data, such as tests and results, is stored locally on your machine.',
+            title: translate({ message: 'Secure' }),
+            description: translate({
+                message:
+                    'Your data, such as tests and results, is stored locally on your machine.',
+            }),
             Svg: require('@site/static/img/icon-secure.svg').default,
         },
         {
-            title: 'Configurable',
-            description:
-                'Hakutest provides an extensive configuration option, allowing you to tailor the experience to your specific needs.',
+            title: translate({ message: 'Configurable' }),
+            description: translate({
+                message:
+                    'Hakutest provides an extensive configuration option, allowing you to tailor the experience to your specific needs.',
+            }),
             Svg: require('@site/static/img/icon-gear.svg').default,
         },
         {
-            title: 'Crossplatform',
-            description:
-                'Prebuilt packages are available for Windows, Linux, and macOS.',
+            title: translate({ message: 'Cross-platform' }),
+            description: translate({
+                message:
+                    'Prebuilt packages are available for Windows, Linux, and macOS.',
+            }),
             Svg: require('@site/static/img/icon-os.svg').default,
         },
     ]
 
     return (
         <section className={styles.homepageFeatures}>
-            <h2 className={styles.featureListTitle}>Features</h2>
+            <h2 className={styles.featureListTitle}>
+                <Translate>Features</Translate>
+            </h2>
             <div className={styles.featureList}>
                 {features.map((feature) => (
                     <Feature {...feature} />
