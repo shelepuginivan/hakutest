50980f85245d78b26c92b2ffc4adfddfb111e0b4
shelepuginivan <shelepuginivanm@gmail.com>
2024-01-31 10:24:31 +0300

    test(results): add tests for results service methods

--- internal/pkg/results/results_test.go
+++ internal/pkg/results/results_test.go
@@ -1,12 +1,55 @@
 package results
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/shelepuginivan/hakutest/internal/config"
 	"github.com/shelepuginivan/hakutest/internal/pkg/test"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestMain(m *testing.M) {
+	defer setup()()
+	m.Run()
+}
+
+func setup() func() {
+	generalConfig := config.New().General
+	testName := "__mock__"
+
+	mockTest := test.Test{
+		Title:  "Mock test",
+		Author: "John Doe",
+		Tasks: []test.Task{
+			{Answer: "A"},
+			{Answer: "B"},
+			{Answer: "C"},
+		},
+	}
+
+	err := test.NewService().SaveToTestsDirectory(mockTest, testName)
+	if err != nil {
+		panic(err)
+	}
+
+	return func() {
+		testPath := filepath.Join(generalConfig.TestsDirectory, testName+".json")
+		resultsPath := filepath.Join(generalConfig.ResultsDirectory, testName)
+
+		err := os.Remove(testPath)
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.RemoveAll(resultsPath)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
 func TestResultsService_CompareAnswers(t *testing.T) {
 	service := NewService()
@@ -59,30 +102,58 @@ func TestResultsService_CheckAnswers(t *testing.T) {
 
 	results := s.CheckAnswers(mockTest, mockAnswers)
 
-	expectedStudent := "ABC"
-	expectedTestInfo := TestInfo{
-		Title:  "Mock Test",
-		Author: "John Doe",
-		Sha256: mockTest.Sha256Sum(),
-	}
-	expectedTaskResults := map[string]TaskResult{
-		"1": {Answer: "A", Correct: true},
-		"2": {Answer: "B", Correct: true},
-		"3": {Answer: "C", Correct: true},
-	}
-	expectedResults := Results{
-		Points:     3,
-		Total:      3,
-		Percentage: 100,
-		Tasks:      expectedTaskResults,
-	}
 	expectedResultsInfo := TestResults{
-		Student:     expectedStudent,
+		Student:     "ABC",
 		SubmittedAt: results.SubmittedAt,
-		Test:        expectedTestInfo,
-		Results:     expectedResults,
+		Test: TestInfo{
+			Title:  mockTest.Title,
+			Author: mockTest.Author,
+			Sha256: mockTest.Sha256Sum(),
+		},
+		Results: Results{
+			Total:      3,
+			Points:     3,
+			Percentage: 100,
+			Tasks: map[string]TaskResult{
+				"1": {Answer: "A", Correct: true},
+				"2": {Answer: "B", Correct: true},
+				"3": {Answer: "C", Correct: true},
+			},
+		},
 	}
 
 	assert.Equal(t, expectedResultsInfo, results)
 	assert.Less(t, expectedResultsInfo.SubmittedAt, time.Now())
+}
+
+func TestResultsService_Save(t *testing.T) {
+	mockResults := TestResults{
+		Student:     "ABC",
+		SubmittedAt: time.Now(),
+		Test: TestInfo{
+			Title:  "Mock test",
+			Author: "John Doe",
+			Sha256: "sha256",
+		},
+		Results: Results{
+			Total:      3,
+			Points:     3,
+			Percentage: 100,
+			Tasks: map[string]TaskResult{
+				"1": {Answer: "A", Correct: true},
+				"2": {Answer: "B", Correct: true},
+				"3": {Answer: "C", Correct: true},
+			},
+		},
+	}
+
+	assert.NoError(t, NewService().Save(mockResults, "__mock__"))
+}
+
+func TestResultsService_GetResultsOfTest(t *testing.T) {
+	results, err := NewService().GetResultsOfTest("__mock__")
+
+	assert.NoError(t, err)
+	assert.Len(t, results, 1)
+	assert.IsType(t, TestResults{}, results[0])
 }
