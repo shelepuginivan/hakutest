2f68ea114e6a9dd1770eb3649cfc4fe7eaa00f72
shelepuginivan <shelepuginivanm@gmail.com>
2024-02-11 17:05:07 +0300

    feat: add package `display`
    Add `display` package that provides methods for printing different data
    types.

---
+++ internal/pkg/display/display.go
@@ -0,0 +1,45 @@
+// Package display provides methods for printing different data types.
+package display
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/rodaine/table"
+)
+
+// PrintMap prints a table representation of a map data structure.
+func PrintMap(m map[string]interface{}) {
+	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
+	columnFmt := color.New(color.FgYellow, color.Bold).SprintfFunc()
+	printKey := color.New(color.FgMagenta, color.Bold).PrintlnFunc()
+
+	tbl := table.New("Key", "Value")
+	tbl.WithHeaderFormatter(headerFmt)
+	tbl.WithFirstColumnFormatter(columnFmt)
+
+	printTable := true
+
+	for key, value := range m {
+		if subMap, ok := value.(map[string]interface{}); ok {
+			printTable = false
+			printKey(key)
+			PrintMap(subMap)
+			fmt.Println()
+			continue
+		}
+
+		tbl.AddRow(key, value)
+	}
+
+	if printTable {
+		tbl.Print()
+	}
+}
+
+// PrintSlice prints slice values, each value on a new line.
+func PrintSlice(s []interface{}) {
+	for _, v := range s {
+		fmt.Println(v)
+	}
+}
