e44402436d8f4e2bad4e0f5aecbd0e74851bad38
shelepuginivan <shelepuginivanm@gmail.com>
2024-02-11 17:06:04 +0300

    feat(config): add config service
    Add struct `ConfigService` that provides config manipulating methods.

---
+++ internal/pkg/config/service.go
@@ -0,0 +1,82 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/shelepuginivan/hakutest/internal/pkg/display"
+	"github.com/spf13/viper"
+)
+
+// options for ConfigService.
+type options struct {
+	v *viper.Viper
+}
+
+// Option represents constructor option for ConfigService.
+type Option func(*options)
+
+// WithViperInstance sets the underlying viper.Viper instance of the ConfigService.
+func WithViperInstance(v *viper.Viper) Option {
+	return func(o *options) {
+		o.v = v
+	}
+}
+
+// ConfigService is a struct that provides methods for manipulating configuration.
+type ConfigService struct {
+	options
+}
+
+// NewService returns a ConfigService instance.
+func NewService(opts ...Option) *ConfigService {
+	var options options
+
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	if options.v == nil {
+		options.v = getViper()
+	}
+
+	return &ConfigService{options: options}
+}
+
+// PrintConfig prints the entire configuration.
+func (s ConfigService) PrintConfig() error {
+	if err := s.v.ReadInConfig(); err != nil {
+		return err
+	}
+
+	display.PrintMap(s.v.AllSettings())
+
+	return nil
+}
+
+// PrintField prints the specified configuration field.
+// If the provided field represents a subbranch, it displays a table representation of this subbranch.
+func (s ConfigService) PrintField(field string) error {
+	if err := s.v.ReadInConfig(); err != nil {
+		return err
+	}
+
+	value := s.v.Get(field)
+
+	if branch, ok := value.(map[string]interface{}); ok {
+		display.PrintMap(branch)
+	} else {
+		fmt.Println(value)
+	}
+
+	return nil
+}
+
+// SetField sets the specified field value.
+func (s ConfigService) SetField(field string, value any) error {
+	if err := s.v.ReadInConfig(); err != nil {
+		return err
+	}
+
+	s.v.Set(field, value)
+	return s.v.WriteConfig()
+}
