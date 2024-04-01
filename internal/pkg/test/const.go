ca0fbff2ec2df21e2a821be8659a20eaced3d0dc
shelepuginivan <shelepuginivanm@gmail.com>
2024-04-01 18:50:13 +0300

    feat(test): add task type `"file"`

--- internal/pkg/test/const.go
+++ internal/pkg/test/const.go
@@ -13,4 +13,5 @@ const (
 	TaskSingle   = "single"   // Single-choice task.
 	TaskMultiple = "multiple" // Multiple-choice task.
 	TaskOpen     = "open"     // Open-ended task.
+	TaskFile     = "file"     // Single or multiple files can be attached as a solution.
 )
