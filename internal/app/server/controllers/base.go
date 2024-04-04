7af79951b884831b41c234fe05e08a51ed993274
shelepuginivan <shelepuginivanm@gmail.com>
2024-04-04 23:57:23 +0300

    feat(controllers): add base method `I18n`
    Add base controller method `I18n` that reads internationalization file
    only once and returns `i18n.I18n` struct.

--- internal/app/server/controllers/base.go
+++ internal/app/server/controllers/base.go
@@ -5,7 +5,17 @@ import (
 	"github.com/shelepuginivan/hakutest/internal/pkg/i18n"
 )
 
-type BaseController struct{}
+type BaseController struct {
+	internationalization *i18n.I18n
+}
+
+func (co BaseController) I18n() *i18n.I18n {
+	if co.internationalization == nil {
+		co.internationalization = i18n.New()
+	}
+
+	return co.internationalization
+}
 
 func (co BaseController) SendErrorResponse(c *gin.Context, code int, err error, detail string) {
 	c.HTML(code, "error.tmpl", gin.H{
