fee49829412e2f5c817296283add9440c32310f9
shelepuginivan <shelepuginivanm@gmail.com>
2024-04-25 17:44:27 +0300

    refactor(components): set label horizontal align

--- internal/app/desktop/components/input.go
+++ internal/app/desktop/components/input.go
@@ -12,30 +12,22 @@ type Input struct {
 func NewInput(label string, field gtk.IWidget) (*Input, error) {
 	var err error
 
-	i := Input{}
+	i := &Input{}
 
 	i.Box, err = gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 4)
 	if err != nil {
 		return nil, err
 	}
-
 	i.SetHAlign(gtk.ALIGN_FILL)
 
-	labelBox, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 0)
+	fieldLabel, err := gtk.LabelNew(label)
 	if err != nil {
 		return nil, err
 	}
+	fieldLabel.SetHAlign(gtk.ALIGN_START)
 
-	gtkLabel, err := gtk.LabelNew(label)
-	if err != nil {
-		return nil, err
-	}
-
-	labelBox.SetHAlign(gtk.ALIGN_START)
-	labelBox.PackStart(gtkLabel, false, false, 0)
-
-	i.PackStart(labelBox, false, false, 0)
+	i.PackStart(fieldLabel, false, false, 0)
 	i.PackStart(field, true, true, 0)
 
-	return &i, nil
+	return i, nil
 }
