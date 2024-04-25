f4a70deddbfde350a17928a16a9bffcaf4754f95
shelepuginivan <shelepuginivanm@gmail.com>
2024-04-26 01:13:23 +0300

    refactor(desktop): remove window from builder

--- internal/app/desktop/builder.go
+++ internal/app/desktop/builder.go
@@ -1,18 +1,13 @@
 package desktop
 
 import (
-	"github.com/gotk3/gotk3/gtk"
 	"github.com/shelepuginivan/hakutest/internal/pkg/application"
 )
 
 type Builder struct {
 	app *application.App
-	win *gtk.Window
 }
 
-func NewBuilder(app *application.App, win *gtk.Window) *Builder {
-	return &Builder{
-		app: app,
-		win: win,
-	}
+func NewBuilder(app *application.App) *Builder {
+	return &Builder{app: app}
 }
