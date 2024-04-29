6ca6e5e13e32f663785448f0ded5e5541630cb68
shelepuginivan <shelepuginivanm@gmail.com>
2024-04-29 17:37:20 +0300

    feat(cmd): change `hakutest-server`
    * Use new internal server api
    * Embed systray icon

--- cmd/hakutest-server/main.go
+++ cmd/hakutest-server/main.go
@@ -2,63 +2,52 @@ package main
 
 import (
 	"context"
+	_ "embed"
 	"net/http"
-	"os"
-	"path/filepath"
 
 	"github.com/getlantern/systray"
 	"github.com/shelepuginivan/hakutest/internal/app/server"
-	"github.com/shelepuginivan/hakutest/internal/pkg/config"
-	"github.com/shelepuginivan/hakutest/internal/pkg/directories"
-	"github.com/shelepuginivan/hakutest/internal/pkg/i18n"
-	"github.com/shelepuginivan/hakutest/internal/pkg/results"
-	"github.com/shelepuginivan/hakutest/internal/pkg/test"
+	"github.com/shelepuginivan/hakutest/internal/pkg/application"
 )
 
-const (
-	appName  = "Hakutest Server"
-	iconPath = "web/static/img/favicon.ico"
-)
+const appName = "Hakutest Server"
+
+//go:embed icon.ico
+var icon []byte
+
+// onReady returns a callback for the systray.Run.
+func onReady(app *application.App) func() {
+	return func() {
+		systray.SetTitle(appName)
+		systray.SetTooltip(appName)
+		systray.SetIcon(icon)
+
+		mStop := systray.AddMenuItem(
+			app.I18n.Server.StopTitle,
+			app.I18n.Server.StopTooltip,
+		)
+
+		srv := server.New(app)
+
+		go func() {
+			err := srv.ListenAndServe()
+			if err != http.ErrServerClosed {
+				panic(err)
+			}
+		}()
+
+		go func() {
+			<-mStop.ClickedCh
+			systray.Quit()
+
+			err := srv.Shutdown(context.Background())
+			if err != nil {
+				panic(err)
+			}
+		}()
+	}
+}
 
 func main() {
-	systray.Run(onReady, func() {})
-}
-
-func getIcon(s string) []byte {
-	b, err := os.ReadFile(s)
-	if err != nil {
-		panic(err)
-	}
-	return b
-}
-
-func onReady() {
-	serverI18n := i18n.New().Server
-
-	systray.SetTitle(appName)
-	systray.SetTooltip(appName)
-	systray.SetIcon(getIcon(filepath.Join(directories.Executable(), iconPath)))
-
-	mStop := systray.AddMenuItem(serverI18n.StopTitle, serverI18n.StopTooltip)
-
-	port := config.New().Server.Port
-	r := server.NewEngine(test.NewService(), results.NewService())
-	srv := server.NewServer(r, port)
-
-	go func() {
-		err := srv.ListenAndServe()
-		if err != http.ErrServerClosed {
-			panic(err)
-		}
-	}()
-
-	go func() {
-		<-mStop.ClickedCh
-		systray.Quit()
-
-		err := srv.Shutdown(context.Background())
-		if err != nil {
-			panic(err)
-		}
-	}()
+	systray.Run(onReady(application.New()), func() {})
 }
