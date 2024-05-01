daf9bb14006c7c0d491f9c0773546ddf2339a370
shelepuginivan <shelepuginivanm@gmail.com>
2024-05-01 12:33:49 +0300

    test(attachment): add test for func `New`

--- internal/pkg/attachment/attachment_test.go
+++ internal/pkg/attachment/attachment_test.go
@@ -1,6 +1,7 @@
 package attachment
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,5 +21,67 @@ func TestIsURL(t *testing.T) {
 
 	for _, c := range cases {
 		assert.Equal(t, c.exp, IsURL(c.src))
+	}
+}
+
+func TestNew(t *testing.T) {
+	bytes := []byte("some text")
+
+	tmp, err := os.CreateTemp("", "*.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = tmp.Write(bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		err = tmp.Close()
+		if err != nil {
+			panic(err)
+		}
+
+		err = os.Remove(tmp.Name())
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	cases := []struct {
+		act *Attachment
+		exp *Attachment
+	}{
+		{
+			act: New(
+				"Some name",
+				AttachmentFile,
+				"https://example.com/file.pdf",
+			),
+			exp: &Attachment{
+				Name: "Some name",
+				Type: AttachmentFile,
+				Src:  "https://example.com/file.pdf",
+			},
+		},
+		{
+			act: New(
+				"Another",
+				AttachmentImage,
+				tmp.Name(),
+			),
+			exp: &Attachment{
+				Name: "Another",
+				Type: AttachmentImage,
+				Src:  "data:text/plain; charset=utf-8;base64,c29tZSB0ZXh0",
+			},
+		},
+	}
+
+	for _, c := range cases {
+		assert.Equal(t, c.exp.Name, c.act.Name)
+		assert.Equal(t, c.exp.Type, c.act.Type)
+		assert.Equal(t, c.exp.Src, c.act.Src)
 	}
 }
