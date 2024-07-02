ba19331f8c34ca60dafccc1261486a5d773c29e5
shelepuginivan <shelepuginivanm@gmail.com>
2024-07-02 18:56:14 +0300

    refactor(web): refactor test page

--- web/js/test.js
+++ web/js/test.js
@@ -1,12 +1,9 @@
 const form = document.getElementById('test-form')
-const modalWrapper = document.querySelector('.modal__wrapper')
+const modal = document.querySelector('.modal')
 const modalBackdrop = document.querySelector('.modal__backdrop')
-const modalConfirm = document.querySelector('.modal__confirm')
-const modalClose = document.querySelector('.modal__close')
 
 function cancel() {
     document.body.classList.remove('modal_open')
-    modalWrapper.classList.remove('visible')
     modalBackdrop.classList.remove('visible')
 }
 
@@ -17,16 +14,23 @@ function submit() {
 form.addEventListener('submit', function(event) {
     event.preventDefault()
     document.body.classList.add('modal_open')
-    modalWrapper.classList.add('visible')
     modalBackdrop.classList.add('visible')
 })
 
-modalConfirm.addEventListener('click', submit)
-modalBackdrop.addEventListener('click', cancel)
-modalClose.addEventListener('click', cancel)
+modal.addEventListener('click', function(event) {
+    event.stopPropagation()
+})
 
 document.body.addEventListener('keydown', function(event) {
     if (event.key === 'Escape') {
         cancel()
     }
+})
+
+Array.from(document.querySelectorAll('[data-cancel]')).forEach(function(el) {
+    el.addEventListener('click', cancel)
+})
+
+Array.from(document.querySelectorAll('[data-confirm]')).forEach(function(el) {
+    el.addEventListener('click', submit)
 })
