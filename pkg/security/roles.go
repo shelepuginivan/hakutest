3af084265ef5137acb0b6a988e20e2d43a34687b
shelepuginivan <shelepuginivanm@gmail.com>
2024-07-05 01:14:57 +0300

    feat(security): add constants for known roles

---
+++ pkg/security/roles.go
@@ -0,0 +1,10 @@
+package security
+
+// Known roles constants.
+const (
+	// Student role. Has access to tests, submissions, results, etc.
+	RoleStudent = "student"
+
+	// Teacher role. Has access to teacher dashboard, settings, etc.
+	RoleTeacher = "teacher"
+)
