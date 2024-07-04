48da9d9f0b16902205b3f6a662ba8096b5982e35
shelepuginivan <shelepuginivanm@gmail.com>
2024-07-05 01:13:34 +0300

    feat(paths): add const for path to encrypted user data

--- internal/pkg/paths/directories.go
+++ internal/pkg/paths/directories.go
@@ -6,7 +6,10 @@ import (
 	"github.com/adrg/xdg"
 )
 
-// Patht to the results directory.
+// Path to the directory with encrypted user data.
+var Users = filepath.Join(xdg.CacheHome, "hakutest", "users")
+
+// Path to the results directory.
 var Results = filepath.Join(xdg.DataHome, "hakutest", "results")
 
 // Path to the tests directory.
