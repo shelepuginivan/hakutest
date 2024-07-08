67142b216e91fd4e88c86a6b5b6b0b0c2a9a59cf
shelepuginivan <shelepuginivanm@gmail.com>
2024-07-08 07:48:09 +0300

    docs(statistics): add godoc comments

--- pkg/statistics/statistics.go
+++ pkg/statistics/statistics.go
@@ -1,13 +1,21 @@
+// Package statistics provides methods for exporting statistics of the testing
+// results.
 package statistics
 
 import "github.com/shelepuginivan/hakutest/pkg/result"
 
+// Statistics is a struct that encapsulates results.
+// Results can be exported to various data formats.
 type Statistics struct {
-	Name    string
+	// Name of the statistics report, typically the same as the test name.
+	Name string
+
+	// Total number of tasks and points.
 	Total   int
 	Results []*result.Result
 }
 
+// New returns a new instance of Statistics.
 func New(testName string, results []*result.Result) *Statistics {
 	s := &Statistics{
 		Name:    testName,
@@ -21,6 +29,9 @@ func New(testName string, results []*result.Result) *Statistics {
 	return s
 }
 
+// NewFromName returns a new instance of Statistics.
+// It reads results from the results directory.
+// E.g. if the testName is `foo`, it reads `/results/directory/foo`.
 func NewFromName(testName string) (*Statistics, error) {
 	results, err := result.GetForTest(testName)
 	if err != nil {
