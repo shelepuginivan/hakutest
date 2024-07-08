3c38e1f7542f615177715e4e4ea599000d29c4c7
shelepuginivan <shelepuginivanm@gmail.com>
2024-07-08 04:37:42 +0300

    feat(fsutil): add method `CreateAll`

--- internal/pkg/fsutil/write.go
+++ internal/pkg/fsutil/write.go
@@ -5,6 +5,18 @@ import (
 	"path/filepath"
 )
 
+// CreateAll creates file and its parent directories.
+// If file exists, it is truncated.
+func CreateAll(file string) (*os.File, error) {
+	dir := filepath.Dir(file)
+
+	if err := os.MkdirAll(dir, os.ModePerm|os.ModeDir); err != nil {
+		return nil, err
+	}
+
+	return os.Create(file)
+}
+
 // WriteAll creates file and all its parent directories.
 // It writes data to the file.
 func WriteAll(file string, data []byte) error {
