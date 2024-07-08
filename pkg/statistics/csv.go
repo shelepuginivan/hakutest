6d96bc0851f970d893592c64820b24c11e9fac2f
shelepuginivan <shelepuginivanm@gmail.com>
2024-07-08 17:11:54 +0300

    feat(statistics): add export to csv methods

---
+++ pkg/statistics/csv.go
@@ -0,0 +1,53 @@
+package statistics
+
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+const (
+	// CSV statistics export format.
+	FormatCSV = "csv"
+
+	// CSV file MIME type.
+	MimeCSV = "text/csv"
+)
+
+// ToCSV returns statistics bytes in CSV format.
+// It calls WriteCSV method of s internally.
+func (s *Statistics) ToCSV() ([]byte, error) {
+	b := bytes.NewBuffer(nil)
+	if err := s.WriteCSV(b); err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+// WriteCSV writes statistics in CSV format to w.
+func (s *Statistics) WriteCSV(w io.Writer) (err error) {
+	csvWriter := csv.NewWriter(w)
+
+	for _, r := range s.Results {
+		record := []string{
+			r.Student,
+			strconv.Itoa(r.Points),
+			strconv.Itoa(r.Percentage),
+		}
+
+		for _, a := range r.Answers {
+			record = append(record, a.Value)
+		}
+
+		if err = csvWriter.Write(record); err != nil {
+			fmt.Println(err)
+			return err
+		}
+	}
+
+	csvWriter.Flush()
+	return csvWriter.Error()
+}
