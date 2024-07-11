c4d8b77d731e55adb40b736d205be938ee2a05af
shelepuginivan <shelepuginivanm@gmail.com>
2024-07-11 17:19:03 +0300

    fix(test): add missing methods

--- pkg/test/methods.go
+++ pkg/test/methods.go
@@ -1,8 +1,10 @@
 package test
 
 import (
+	"archive/zip"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -63,4 +65,94 @@ func GetByName(name string) (*Test, error) {
 	}
 
 	return &test, nil
+}
+
+// Import imports test to the tests directory.
+func Import(test []byte) error {
+	var t Test
+	if err := json.Unmarshal(test, &t); err != nil {
+		return err
+	}
+
+	if strings.TrimSpace(t.Title) == "" {
+		return fmt.Errorf("title must not be empty")
+	}
+
+	testPath := filepath.Join(testsDirectory, NormalizeName(t.Title))
+
+	if !fsutil.FileExists(testPath) {
+		return fsutil.WriteAll(testPath, test)
+	}
+
+	// If test with this name exists, append numeric suffix.
+	// The suffix is incremented until test name is unique.
+	for i := 1; true; i++ {
+		testName := fmt.Sprintf("%s (%d)", t.Title, i)
+		testPath = filepath.Join(testsDirectory, NormalizeName(testName))
+
+		if !fsutil.FileExists(testPath) {
+			return fsutil.WriteAll(testPath, test)
+		}
+	}
+
+	return nil
+}
+
+// WriteJSON writes raw test JSON file to w.
+func WriteJSON(w io.Writer, name string) error {
+	t, err := GetByName(name)
+	if err != nil {
+		return err
+	}
+
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(data)
+	return err
+}
+
+// WriteZip appends tests by names to a zip archive and writes the resulting
+// archive to w.
+func WriteZip(w io.Writer, names ...string) error {
+	zipWriter := zip.NewWriter(w)
+
+	for _, name := range names {
+		f, err := zipWriter.Create(NormalizeName(name))
+		if err != nil {
+			continue
+		}
+
+		t, err := GetByName(name)
+		if err != nil {
+			continue
+		}
+
+		data, err := json.Marshal(t)
+		if err != nil {
+			continue
+		}
+
+		f.Write(data)
+	}
+
+	return zipWriter.Close()
+}
+
+// DeleteMany deletes tests by names. The returned value is the number of
+// successfully deleted test files.
+func DeleteMany(names ...string) (deleted int) {
+	for _, name := range names {
+		err := os.Remove(filepath.Join(testsDirectory, NormalizeName(name)))
+
+		if err != nil {
+			continue
+		}
+
+		deleted++
+	}
+
+	return deleted
 }
